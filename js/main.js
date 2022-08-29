c48ffb38c96dbe555ddcd9f582f2aa67dd8401d0
uknown-me <64226929+uknown-me@users.noreply.github.com>
2022-08-30 00:22:24 +0400

    add new inputs files and css propertys

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// const button1 = document.querySelector(".button1");
+// const hoverBackLine = document.querySelector(".hoverblackline");
+// (userName = document.querySelector("#username")),
+//   function validateUserName() {
+//     // როცა ვალიდური არაა, ეს required არის და ცარიელი როცაა მაშინ გამოვა ერორი
+//     if (!userName.validity.valid) {
+//       // console.log(userName.validity);
+//       userName.classList.add("error");
+//       userNameError.textContent = "user name required";
+
+//       // true ან დაბლა false ს ვაბრუნებთ იმის მიხედვით ვალიდაცია გაიარა თუ არა
+//       return false;
+//     } else {
+//       userNameError.textContent = "";
+//       userName.classList.remove("error");
+//       return true;
+//     }
+//   };
