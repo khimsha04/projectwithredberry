38fc6f2e16f75aed2b812e6b5a71f0b67c8c3304
uknown-me <64226929+uknown-me@users.noreply.github.com>
2022-09-03 15:34:26 +0400

    update js and css files

--- js/api.js
+++ js/api.js
@@ -1,78 +1,78 @@
-// document.addEventListener(
-//   "DOMContentLoaded",
-//   function () {
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("GET", "https://pcfy.redberryinternship.ge/api/teams");
-//     xhr.onloadend = function () {
-//       let response = JSON.parse(xhr.response);
-//       for (i = 0; i < response.data.length; i++) {
-//         let tempdocument = document.createElement("option");
-//         tempdocument.classList.add("menu1value");
-//         tempdocument.value = response.data[i].id;
-//         tempdocument.innerText = response.data[i].name;
-//         team.append(tempdocument);
-//       }
-//     };
-//     xhr.send();
-//   },
-//   false
-// );
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://pcfy.redberryinternship.ge/api/teams");
+    xhr.onloadend = function () {
+      let response = JSON.parse(xhr.response);
+      for (i = 0; i < response.data.length; i++) {
+        let tempdocument = document.createElement("option");
+        tempdocument.classList.add("menu1value");
+        tempdocument.value = response.data[i].id;
+        tempdocument.innerText = response.data[i].name;
+        team.append(tempdocument);
+      }
+    };
+    xhr.send();
+  },
+  false
+);
 
-// document.addEventListener(
-//   "DOMContentLoaded",
-//   function () {
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("GET", "https://pcfy.redberryinternship.ge/api/positions");
-//     xhr.onloadend = function () {
-//       let response = JSON.parse(xhr.response);
-//       for (i = 0; i < response.data.length; i++) {
-//         let tempdocument = document.createElement("option");
-//         tempdocument.classList.add("menu1value");
-//         tempdocument.value = response.data[i].id;
-//         tempdocument.innerText = response.data[i].name;
-//         position.append(tempdocument);
-//       }
-//     };
-//     xhr.send();
-//   },
-//   false
-// );
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://pcfy.redberryinternship.ge/api/positions");
+    xhr.onloadend = function () {
+      let response = JSON.parse(xhr.response);
+      for (i = 0; i < response.data.length; i++) {
+        let tempdocument = document.createElement("option");
+        tempdocument.classList.add("menu1value");
+        tempdocument.value = response.data[i].id;
+        tempdocument.innerText = response.data[i].name;
+        position.append(tempdocument);
+      }
+    };
+    xhr.send();
+  },
+  false
+);
 
-// document.addEventListener(
-//   "DOMContentLoaded",
-//   function () {
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("GET", "https://pcfy.redberryinternship.ge/api/brands");
-//     xhr.onloadend = function () {
-//       let response = JSON.parse(xhr.response);
-//       for (i = 0; i < response.data.length; i++) {
-//         let tempdocument = document.createElement("option");
-//         tempdocument.classList.add("menu1value");
-//         tempdocument.value = response.data[i].id;
-//         tempdocument.innerText = response.data[i].name;
-//         brands.append(tempdocument);
-//       }
-//     };
-//     xhr.send();
-//   },
-//   false
-// );
-// document.addEventListener(
-//   "DOMContentLoaded",
-//   function () {
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("GET", "https://pcfy.redberryinternship.ge/api/cpus");
-//     xhr.onloadend = function () {
-//       let response = JSON.parse(xhr.response);
-//       for (i = 0; i < response.data.length; i++) {
-//         let tempdocument = document.createElement("option");
-//         tempdocument.classList.add("menu2value");
-//         tempdocument.value = response.data[i].id;
-//         tempdocument.innerText = response.data[i].name;
-//         cpu.append(tempdocument);
-//       }
-//     };
-//     xhr.send();
-//   },
-//   false
-// );
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://pcfy.redberryinternship.ge/api/brands");
+    xhr.onloadend = function () {
+      let response = JSON.parse(xhr.response);
+      for (i = 0; i < response.data.length; i++) {
+        let tempdocument = document.createElement("option");
+        tempdocument.classList.add("menu1value");
+        tempdocument.value = response.data[i].id;
+        tempdocument.innerText = response.data[i].name;
+        brands.append(tempdocument);
+      }
+    };
+    xhr.send();
+  },
+  false
+);
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://pcfy.redberryinternship.ge/api/cpus");
+    xhr.onloadend = function () {
+      let response = JSON.parse(xhr.response);
+      for (i = 0; i < response.data.length; i++) {
+        let tempdocument = document.createElement("option");
+        tempdocument.classList.add("menu2value");
+        tempdocument.value = response.data[i].id;
+        tempdocument.innerText = response.data[i].name;
+        cpu.append(tempdocument);
+      }
+    };
+    xhr.send();
+  },
+  false
+);
--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,6 @@
 const brands = document.getElementById("menu3");
 const cpu = document.getElementById("menu4");
+const upload = document.getElementById("fileupload");
 const laptopName = document.getElementById("laptopname");
 const cpuCore = document.getElementById('"CPU-S');
 const cpuStream = document.getElementById("cpustream");
@@ -7,8 +8,12 @@ const laptopRam = document.getElementById("laptopram");
 const dayBuy = document.getAnimations("date");
 const price = document.getElementById("price");
 const otherWord = "!@#$%^&*()_+=";
+const uploadButton = document.getElementById("button-upload");
 const saveButton = document.querySelector(".save-button");
-
+uploadButton.addEventListener("click", function () {
+  let input = document.getElementById("fileupload");
+  input.click();
+});
 saveButton.addEventListener("click", function () {
   let brandsValue = brands.value,
     cpuValue = cpu.value,
@@ -17,9 +22,19 @@ saveButton.addEventListener("click", function () {
     spuStreamValue = cpuStream.value,
     laptopRamValue = laptopRam.value,
     dayBuyValue = dayBuy.value,
-    priceValue = price.value;
+    priceValue = price.value,
+    uploadValue = upload.value;
+  let isVallid = true;
 
+  if (brandsValue == -1) {
+    showErrorMessage(brands);
+    isVallid = false;
+  }
+  if (cpuValue == -1) {
+    showErrorMessage(cpu);
+    isVallid = false;
+  }
   if (isVallid) {
-    window.location.href = "/blog.html";
+    window.location.href = "/popup.html";
   }
 });
