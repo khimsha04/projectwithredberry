e8b1733d3e285f34e4afa21680b029fbd6f1779f
uknown-me <64226929+uknown-me@users.noreply.github.com>
2022-09-04 12:44:04 +0400

    api js

--- js/api.js
+++ js/api.js
@@ -38,59 +38,59 @@ document.addEventListener(
   false
 );
 
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://pcfy.redberryinternship.ge/api/brands");
-    xhr.onloadend = function () {
-      let response = JSON.parse(xhr.response);
-      for (i = 0; i < response.data.length; i++) {
-        let tempdocument = document.createElement("option");
-        tempdocument.classList.add("menu1value");
-        tempdocument.value = response.data[i].id;
-        tempdocument.innerText = response.data[i].name;
-        brands.append(tempdocument);
-      }
-    };
-    xhr.send();
-  },
-  false
-);
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://pcfy.redberryinternship.ge/api/cpus");
-    xhr.onloadend = function () {
-      let response = JSON.parse(xhr.response);
-      for (i = 0; i < response.data.length; i++) {
-        let tempdocument = document.createElement("option");
-        tempdocument.classList.add("menu2value");
-        tempdocument.value = response.data[i].id;
-        tempdocument.innerText = response.data[i].name;
-        cpu.append(tempdocument);
-      }
-    };
-    xhr.send();
-  },
-  false
-);
+// document.addEventListener(
+//   "DOMContentLoaded",
+//   function () {
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("GET", "https://pcfy.redberryinternship.ge/api/brands");
+//     xhr.onloadend = function () {
+//       let response = JSON.parse(xhr.response);
+//       for (i = 0; i < response.data.length; i++) {
+//         let tempdocument = document.createElement("option");
+//         tempdocument.classList.add("menu1value");
+//         tempdocument.value = response.data[i].id;
+//         tempdocument.innerText = response.data[i].name;
+//         brands.append(tempdocument);
+//       }
+//     };
+//     xhr.send();
+//   },
+//   false
+// );
+// document.addEventListener(
+//   "DOMContentLoaded",
+//   function () {
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("GET", "https://pcfy.redberryinternship.ge/api/cpus");
+//     xhr.onloadend = function () {
+//       let response = JSON.parse(xhr.response);
+//       for (i = 0; i < response.data.length; i++) {
+//         let tempdocument = document.createElement("option");
+//         tempdocument.classList.add("menu2value");
+//         tempdocument.value = response.data[i].id;
+//         tempdocument.innerText = response.data[i].name;
+//         cpu.append(tempdocument);
+//       }
+//     };
+//     xhr.send();
+//   },
+//   false
+// );
 
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://pcfy.redberryinternship.ge/api/laptops");
-    xhr.onloadend = function () {
-      let response = JSON.parse(xhr.response);
-      console.log(response);
-    };
-    xhr.setRequestHeader(
-      "Authorization",
-      "Bearer 38289d30bb6ae45f473c7273741dd54f"
-    );
-    xhr.send();
-  },
-  false
-);
+// document.addEventListener(
+//   "DOMContentLoaded",
+//   function () {
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("GET", "https://pcfy.redberryinternship.ge/api/laptops");
+//     xhr.onloadend = function () {
+//       let response = JSON.parse(xhr.response);
+//       console.log(response);
+//     };
+//     xhr.setRequestHeader(
+//       "Authorization",
+//       "Bearer 38289d30bb6ae45f473c7273741dd54f"
+//     );
+//     xhr.send();
+//   },
+//   false
+// );
